6bf7b050ff0b37854d8bfa27c6de61f2e129658a
eve-le-guillou <eve.leguillou@protonmail.com>
2022-12-01 16:59:11 +0100

    Attempt at solving padding

--- core/base/common/ArrayLinkedList.h
+++ core/base/common/ArrayLinkedList.h
@@ -12,13 +12,13 @@ namespace ttk {
   template <typename datatype, int size>
   class ArrayLinkedList {
   public:
-    std::list<std::array<datatype, size>> list;
     // In order to prevent false sharing when creating a
     // std::vector of ArrayLinkedList objects (one element
     // of the std::vector for each thread), it is necessary
     // that one ArrayLinkedList object be bigger than one cache
     // line. Here, we assume a cache line to be 64 bytes.
     int numberOfElement __attribute__((aligned(64)));
+    std::list<std::array<datatype, size>> list;
 
     ArrayLinkedList()
       : list(std::list<std::array<datatype, size>>({})), numberOfElement(0) {
